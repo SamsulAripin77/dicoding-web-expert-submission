fcd3e9e17b621bcb6e8920cb8a6c3c64cc11316e
SamsulAripin77 <arifins192@gmail.com>
2021-07-01 13:38:21 +0700

    index.html tidak ter cache

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   IMAGE_URL: 'https://restaurant-api.dicoding.dev/images',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
