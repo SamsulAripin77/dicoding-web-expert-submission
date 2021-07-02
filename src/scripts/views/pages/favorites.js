bc7b6689f43d3af98a4effc57d7f90bb18a66b87
SamsulAripin77 <arifins192@gmail.com>
2021-07-02 16:48:10 +0700

    halaman favorite ok

--- src/scripts/views/pages/favorites.js
+++ src/scripts/views/pages/favorites.js
@@ -1,12 +1,25 @@
+import FavoriteRestaurantIdb from "../../data/favoriterestaurant-idb";
+import { createMenuItemTemplate } from "../../templates/template-creator";
+
 const Favorites = {
   async render() {
     return `
-        <h2>Favorites</h2>
+        <div class="content">
+          <h2 class="content_heading">Your favorite restaurant</h2>
+          <div id="list-rest" class="menus">
+        
+          </div>
+        </div>
       `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurant = await FavoriteRestaurantIdb.getAllRestaurant();
+    const restaurantContainer = document.querySelector('#list-rest');
+    restaurant.forEach((restaurant) => {
+      console.log(restaurant);
+      restaurantContainer.innerHTML += createMenuItemTemplate(restaurant)
+    })
   },
 };
 
